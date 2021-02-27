562a9651a6798442405d344f3e3b312f7a19100a
Josiah Mendes <josiahmendes@icloud.com>
2021-02-27 16:30:11 +0000

    Exercise 1.17 nicer formatting

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -20,7 +20,6 @@ while principal > payment:
     if (month >= extra_payment_start_month and month <= extra_payment_end_month):
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
-    print(month, round(total_paid,2), round(principal,2))
-
+    print(f'{month:3}{round(principal,2):15f}{round(total_paid,2):15f}')
 print('Total paid',round(total_paid,1))
 print('Months',month)
