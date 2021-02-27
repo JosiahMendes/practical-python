6aecc0089d280bf18686f27ab6e9056b52fca137
Josiah Mendes <josiahmendes@icloud.com>
2021-02-27 15:36:14 +0000

    Exercise 1.10 Done

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -20,7 +20,7 @@ while principal > 0:
     if (month >= extra_payment_start_month and month <= extra_payment_end_month):
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
-
+    print(month, round(total_paid,2), round(principal,2))
 
 print('Total paid',round(total_paid,1))
-print('Number of months',month)
+print('Months',month)
